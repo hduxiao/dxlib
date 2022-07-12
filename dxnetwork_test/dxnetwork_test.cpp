31e59b330a865c5b213c5152a28d661bdc2d6a17
dxiao <hduxiao@outlook.com>
2022-07-12 21:52:02 +0800

    add command line

--- dxnetwork_test/dxnetwork_test.cpp
+++ dxnetwork_test/dxnetwork_test.cpp
@@ -5,8 +5,13 @@
 #define GetProcAddress dlsym
 #define FreeLibrary dlclose
 #endif
+
 #include <thread>
+#include <iostream>
 #include "../dxnetwork/interface.h"
+
+#define SEND_MODE 0
+#define RECEIVE_MODE 1
 
 void test_listen(i_dxnetwork_transfer* pTransfer)
 {
@@ -17,11 +22,18 @@ void test_listen(i_dxnetwork_transfer* pTransfer)
 void test_connect(i_dxnetwork_transfer* pTransfer)
 {
 	int connect_result = 0;
-	pTransfer->connect("192.168.0.100", 5127, connect_result);
+	pTransfer->connect("192.168.0.102", 5127, connect_result);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc !=  2 || ((std::string(argv[1]) != "-send") && (std::string(argv[1]) != "-receive"))) {
+        std::cout << "How are you?\n";
+        return 0;
+    }
+
+    int workMode = (std::string(argv[1]) == "-send") ? SEND_MODE : RECEIVE_MODE;
+
 	i_dxnetwork_object* pObject = nullptr;
 	i_dxnetwork_factory* pFactory = nullptr;
 	i_dxnetwork_transfer* pTransfer = nullptr;
@@ -51,11 +63,16 @@ int main()
 
 	if (pTransfer)
 	{
-		std::thread th_listen(test_listen, pTransfer);
-//		std::thread th_connect(test_connect, pTransfer);
-
-//		th_connect.join();
-		th_listen.join();
+        if (workMode == SEND_MODE)
+        {
+            std::thread th_listen(test_listen, pTransfer);
+            th_listen.join();
+        }
+		else if (workMode == RECEIVE_MODE)
+        {
+            std::thread th_connect(test_connect, pTransfer);
+		    th_connect.join();
+        }
 
 		delete pTransfer;
 		delete pFactory;
